cb409b4b545fd1029f32f67f507e070bc9abf0d6
gabor.soros <gabor.soros@gmail.com>
2023-04-26 17:41:58 +0200

    cmake adaptations in preparation for ROS2 wrapper

--- build.sh
+++ build.sh
@@ -1,57 +1,45 @@
 # Set this path. Dependencies are assumed to be installed to this folder:
 #ORBSLAM3_DEPENDENCIES_INSTALL_DIR="/home/esoptron/dev/slamtest/install"
-ORBSLAM3_DEPENDENCIES_INSTALL_DIR=""
+ORBSLAM3_DEPENDENCIES_INSTALL_DIR="/home/esoptron/dev/navigation_ws/install"
 echo "ORBSLAM3_DEPENDENCIES_INSTALL_DIR=$ORBSLAM3_DEPENDENCIES_INSTALL_DIR"
 
-
-echo "Configuring and building Thirdparty/DBoW2 ..."
-
-cd Thirdparty/DBoW2
-mkdir build
-cd build
-cmake .. \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_PREFIX_PATH=$ORBSLAM3_DEPENDENCIES_INSTALL_DIR
-
-make -j
-
-cd ../../g2o
-
-echo "Configuring and building Thirdparty/g2o ..."
-
-mkdir build
-cd build
-cmake .. \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_PREFIX_PATH=$ORBSLAM3_DEPENDENCIES_INSTALL_DIR
-
-make -j
-
-cd ../../Sophus
-
-echo "Configuring and building Thirdparty/Sophus ..."
-
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
-
-cd ../../../
+#echo "Configuring and building Thirdparty/DBoW2 ..."
+#cd Thirdparty/DBoW2
+#mkdir build
+#cd build
+#cmake .. \
+#  -DCMAKE_BUILD_TYPE=Release \
+#  -DCMAKE_PREFIX_PATH=$ORBSLAM3_DEPENDENCIES_INSTALL_DIR
+#make -j2
+#
+#echo "Configuring and building Thirdparty/g2o ..."
+#cd ../../g2o
+#mkdir build
+#cd build
+#cmake .. \
+#  -DCMAKE_BUILD_TYPE=Release \
+#  -DCMAKE_PREFIX_PATH=$ORBSLAM3_DEPENDENCIES_INSTALL_DIR
+#make -j2
+#
+#echo "Configuring and building Thirdparty/Sophus ..."
+#cd ../../Sophus
+#mkdir build
+#cd build
+#cmake .. -DCMAKE_BUILD_TYPE=Release
+#make -j2
+#cd ../../../
 
 echo "Uncompress vocabulary ..."
-
 cd Vocabulary
 tar -xf ORBvoc.txt.tar.gz
 cd ..
 
 echo "Configuring and building ORB_SLAM3 ..."
-
 mkdir build
 cd build
 cmake .. \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_PREFIX_PATH=$ORBSLAM3_DEPENDENCIES_INSTALL_DIR
-
-make -j
-
+make -j2
 cd ..
+
